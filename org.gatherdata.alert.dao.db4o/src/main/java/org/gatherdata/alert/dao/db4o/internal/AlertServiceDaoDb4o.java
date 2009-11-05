96fd0542bffc0d843f2109dc05aa1bd34c094ecb
Gather Data <gather@tembopublic.org>
2009-11-05 10:29:12 -0500

    added license note to headers

--- org.gatherdata.alert.dao.db4o/src/main/java/org/gatherdata/alert/dao/db4o/internal/AlertServiceDaoDb4o.java
+++ org.gatherdata.alert.dao.db4o/src/main/java/org/gatherdata/alert/dao/db4o/internal/AlertServiceDaoDb4o.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the AED Public Use License Agreement, Version 1.0 (the "License");
+ * use in any manner is strictly prohibited except in compliance with the terms of the License.
+ * The License is available at http://gatherdata.org/license.
+ *
+ * Copyright (c) AED.  All Rights Reserved
+ */
 package org.gatherdata.alert.dao.db4o.internal;
 
 import java.net.URI;
--- org.gatherdata.alert.notify.mail/src/main/java/org/gatherdata/alert/notify/mail/osgi/GuiceBindingModule.java
+++ org.gatherdata.alert.notify.mail/src/main/java/org/gatherdata/alert/notify/mail/osgi/GuiceBindingModule.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the AED Public Use License Agreement, Version 1.0 (the "License");
+ * use in any manner is strictly prohibited except in compliance with the terms of the License.
+ * The License is available at http://gatherdata.org/license.
+ *
+ * Copyright (c) AED.  All Rights Reserved
+ */
 package org.gatherdata.alert.notify.mail.osgi;
 
 import static org.ops4j.peaberry.Peaberry.service;
